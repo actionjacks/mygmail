cc29098c5d68a9e2bc439fcfba626ca2da62de55
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-03-27 18:47:02 +0100

    add firebase connection config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAzdVLPD2ZQVpnDu_0hTV4GDOW99QLkMpY",
+  authDomain: "fake-23826.firebaseapp.com",
+  projectId: "fake-23826",
+  storageBucket: "fake-23826.appspot.com",
+  messagingSenderId: "1007029746871",
+  appId: "1:1007029746871:web:440a34554a98a484d1e132",
+  measurementId: "G-DVK8YRS1JT",
+};
