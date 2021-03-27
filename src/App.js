31027df9e491a2540beb38fc9f56ebf55b9f94a1
jacek zablocki <56847035+actionjacks@users.noreply.github.com>
2021-03-27 18:39:24 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//cmponents
+import React from "react";
+import Header from "./components/Header";
+//styles
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
